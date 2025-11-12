d529fe1a3b05e29fe89040975c0296ddefda2372
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-11-12 14:39:56 +0100

    created file for constants

---
+++ src/const.h
@@ -0,0 +1,26 @@
+// ultrasonic sensor 1 pinout
+const int Trig = 2;
+const int Echo = 3;
+
+// ultrasonic sensor 2 pinout
+
+// ultrasonic sensor 3 pinout
+
+// ultrasonic sensor 4 pinout
+
+// stepper motor pinout
+const int IN1 = 8;
+const int IN2 = 9;
+const int IN3 = 10;
+const int IN4 = 11;
+
+// stepper motor resolutions
+
+const int maxResolution = 4096;
+const int minResolution = 0;
+
+// time 
+int CM;
+long TIME;
+
+
