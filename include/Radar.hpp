5cead52e80a88443fe4464e1f678794251ecf54b
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-11-18 15:26:12 +0100

    add reciving data from gui; add method for changing parameters

--- include/Radar.hpp
+++ include/Radar.hpp
@@ -2,18 +2,24 @@
 
 #include "StepperMotor.hpp"
 #include "Sensor.hpp"
+#include <ArduinoJson.h>
 
 class Radar{
     private:
         StepperMotor &_motor;
         Sensor &_sensor;
         int _sampleCount;
+        String _recivedInfo;
+        float _recivedValue;
+        int _measureTime;
 
     public:
-        Radar(StepperMotor &motor, Sensor &sensor): _motor(motor), _sensor(sensor){
+        Radar(StepperMotor &motor, Sensor &sensor): _motor(motor), _sensor(sensor), _recivedInfo(""){
             _sampleCount = static_cast<int>(180.0f / motor.getStepAngle()) + 1;
         }
         void init(const float maxSpeed, const float acceleration);
         void scanSweep(bool forward);
         void emitPoint(float angle, uint16_t distance) const;
+        bool reciveData();
+        void changeParameters();
 };
--- src/Radar.cpp
+++ src/Radar.cpp
@@ -11,8 +11,14 @@ void Radar::scanSweep(bool forward){
         const int idx = forward ? i : (_sampleCount - 1 - i);
         _motor.moveStepperToSample(idx);
         float angle = idx * stepAngle;
-        uint16_t dist = _sensor.measureCm();
-        emitPoint(angle, dist);
+        unsigned long startTime = millis();
+        unsigned long elapsed = startTime;
+        while (elapsed - startTime > _measureTime)
+        {
+            uint16_t dist = _sensor.measureCm();
+            emitPoint(angle, dist);
+            elapsed = millis();
+        }
         delay(30);
     }
     Serial.println("END");
@@ -22,4 +28,33 @@ void Radar::emitPoint(float angle, uint16_t distance) const{
     Serial.print(angle, 1);
     Serial.print(',');
     Serial.println(distance);
+}
+
+bool Radar::reciveData(){
+    if (!Serial.available())
+        return false;
+
+    String recivedString = Serial.readStringUntil('\n');
+    
+    if (recivedString.length() == 0)
+        return false;
+
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, recivedString);
+
+    if (!error){
+        _recivedInfo = doc["Command"].as<String>();
+        _recivedValue = doc["Value"].as<int>(); 
+    }
+
+    return true;
+}
+
+void Radar::changeParameters(){
+    if(reciveData()){
+        if(_recivedInfo == "Resolution")      
+            _motor.changeResolution(_recivedValue);
+        else if (_recivedInfo == "Time")
+            _measureTime = _recivedValue;
+    }
 }
